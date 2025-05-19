c0171615f2478c3d536a45c187e771e6aafb2da8
aluqmor <alvarolu99@gmail.com>
2025-05-19 13:28:06 +0200

    Estilos de la página principal y añadir iconos

--- proyecto-integrado/src/pages/main/MainPage.tsx
+++ proyecto-integrado/src/pages/main/MainPage.tsx
@@ -18,9 +18,10 @@ export function MainPage() : JSX.Element {
           <nav className="navigation">
             <ul>
               <li><Link to="/">Inicio</Link></li>
+              <li> <Link to="/play">Jugar</Link></li>
               <li> <Link to="/join">Unirse a partida</Link></li>
-              <li> <Link to="/create">Crear test</Link></li>
               <li> <Link to="/host">Organizar partida</Link></li>
+              <li> <Link to="/create">Crear test</Link></li>
               <li>
                 <Link to="/user">
                   <img src="../src/assets/images/user.png" alt="User Sign In" className="user-icon" />
@@ -36,23 +37,26 @@ export function MainPage() : JSX.Element {
       </div>
       <div className="about-us">
         <div className="about-text">
-          <h2>¿Qué es Quizify?</h2>
+          <h2>¿Qué es <span>Quizify</span>?</h2>
           <p>
-            Quizify es una plataforma de aprendizaje interactivo que permite a los usuarios crear, compartir y 
-            jugar cuestionarios en línea. Con una interfaz intuitiva y una amplia variedad de temas, Quizify hace 
-            que el aprendizaje sea divertido y accesible para todos. Ya sea que estés estudiando para un examen o 
-            simplemente quieras poner a prueba tus conocimientos, Quizify es la herramienta perfecta para ti.
+            Es una plataforma de <span>aprendizaje interactivo</span> que permite a los usuarios <span>crear</span>, <span>compartir</span> y <span>jugar</span> cuestionarios en línea. Con una interfaz intuitiva y una amplia 
+            <span>variedad de temas</span>, Quizify hace que el aprendizaje sea <span>divertido</span> y <span>accesible</span> para todos. 
+            Ya sea que estés estudiando para un examen o simplemente quieras poner a prueba tus conocimientos, 
+            Quizify es la <span>herramienta perfecta</span> para ti.
           </p>
+          <button><Link to="/play"><p>¡Empieza ahora!</p></Link></button>
         </div>
         <div className="about-img"></div>
       </div>
-      <div className="counter">
-        <button> <Link to="/join">Join Game</Link>
-        </button>
-        <button> <Link to="/create">Create test</Link>
-        </button>
-        <button> <Link to="/host">Host game</Link>
-        </button>
+      <div className="quote-section">
+        <p className="quote">“We learn from failure, not from success”</p>
+        <p className="author">— Bram Stoker</p>
+      </div>
+      <div className="play-section">
+        <h2>¿Cómo jugar?</h2>
+        <p>Hay diferentes</p>
+        <button> <Link to="/join"><p>Unirse a partida</p></Link></button>
+        <button> <Link to="/host"><p>Organizar partida</p></Link></button>
       </div>
       <div className ="cards-section">
       </div>
